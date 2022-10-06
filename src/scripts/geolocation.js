7335eb959ab0016d5d7a3520e87c52bec9448d2b
Zhile Lin <zhlin04@gmail.com>
2022-10-06 09:21:58 -0700

    flex pos

--- src/scripts/geolocation.js
+++ src/scripts/geolocation.js
@@ -6,39 +6,39 @@ function findGeo(map,infoWindow,directionsService,directionsRenderer,data,latitu
       directionsRenderer.setMap(map); // object that set route when fullfilled request
     
         let bIdx = Math.floor(Math.random() * 20);
-        let pos={lat:latitude,lng:longitude}
-        
+        let pos = {lat:latitude,lng:longitude}
+        console.log('what is pos here in findGeo',pos)
          const destination = {
             lat: data.businesses[bIdx].coordinates.latitude,
             lng: data.businesses[bIdx].coordinates.longitude,
           };
-              if(navigator.geolocation){
-                    navigator.geolocation.getCurrentPosition(
-                            (position) =>{
-                              const pos = {
-                                lat: position.coords.latitude,
-                                lng: position.coords.longitude,
-                              };
-                              infoWindow.setPosition(destination);
-                              infoWindow.setContent(`${data.businesses[bIdx].name}`);
-                              infoWindow.open(map);
-                              map.setCenter(destination);
-                              calcRoute(pos,destination,directionsService,directionsRenderer);
-                            },
-                            () => {
-                              handleLocationError(true, infoWindow, map.getCenter());
-                            }
-                    );
-              }else{
-                   // Browser doesn't support Geolocation
-               handleLocationError(false, infoWindow, map.getCenter());
-              }
+        //       if(navigator.geolocation){
+        //             navigator.geolocation.getCurrentPosition(
+        //                     (position) =>{
+        //                       const pos = {
+        //                         lat: position.coords.latitude,
+        //                         lng: position.coords.longitude,
+        //                       };
+        //                       infoWindow.setPosition(destination);
+        //                       infoWindow.setContent(`${data.businesses[bIdx].name}`);
+        //                       infoWindow.open(map);
+        //                       map.setCenter(destination);
+        //                       calcRoute(pos,destination,directionsService,directionsRenderer);
+        //                     },
+        //                     () => {
+        //                       handleLocationError(true, infoWindow, map.getCenter());
+        //                     }
+        //             );
+        //       }else{
+        //            // Browser doesn't support Geolocation
+        //        handleLocationError(false, infoWindow, map.getCenter());
+        //       }
 //       })
-          // infoWindow.setPosition(destination);
-          // infoWindow.setContent(`${data.businesses[bIdx].name}`);
-          // infoWindow.open(map);
-          // map.setCenter(destination);
-          // calcRoute(pos,destination,directionsService,directionsRenderer);
+          infoWindow.setPosition(destination);
+          infoWindow.setContent(`${data.businesses[bIdx].name}`);
+          infoWindow.open(map);
+          map.setCenter(destination);
+          calcRoute(pos,destination,directionsService,directionsRenderer);
           }
 
 export default findGeo;
